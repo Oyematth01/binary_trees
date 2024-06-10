77a3a4e667d0aed53f18faf6618a50f085c77b93
Oyematth01 <oyematth@gmail.com>
2024-06-10 01:46:54 +0100

    task 27

---
+++ 113-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    bst_t *tree;
+    bst_t *node;
+    int array[] = {
+        98, 402, 12, 46, 128, 256, 512, 1, 128, 98
+    };
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    tree = array_to_bst(array, n);
+    if (tree)
+        binary_tree_print(tree);
+
+    node = bst_search(tree, 128);
+    if (node)
+        printf("Found: %d\n", node->n);
+    else
+        printf("Value not found\n");
+
+    node = bst_search(tree, 1000);
+    if (node)
+        printf("Found: %d\n", node->n);
+    else
+        printf("Value not found\n");
+
+    return (0);
+}
